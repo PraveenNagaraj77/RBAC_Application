51ce568be6529dba2c57978d801daf48780bf401
PraveenNagaraj77 <praveennagaraj76@gmail.com>
2025-06-05 23:23:33 +0530

    Frontend Changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
 import './index.css'
-=======
->>>>>>> 9770381 (backend changes updated)
 import App from './App.jsx'
 import './index.css';
 
